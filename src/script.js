37c38b66232d8616e43b72d3afd4ea9f3d8be996
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-11-05 11:00:19 +0100

    added textures to roof

--- src/script.js
+++ src/script.js
@@ -49,6 +49,34 @@ const wallARMTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/cast
 const wallNormalTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_gl_1k.webp')
 
 wallColorTexture.colorSpace = THREE.SRGBColorSpace
+
+//Roof Texture
+const roofColorTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_diff_1k.webp')
+const roofARMTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_arm_1k.webp')
+const roofNormalTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_nor_gl_1k.webp')
+
+roofColorTexture.colorSpace = THREE.SRGBColorSpace
+
+roofColorTexture.repeat.set(3, 1)
+roofARMTexture.repeat.set(3, 1)
+roofNormalTexture.repeat.set(3, 1)
+
+roofColorTexture.wrapS = THREE.RepeatWrapping
+roofARMTexture.wrapS = THREE.RepeatWrapping
+roofNormalTexture.wrapS = THREE.RepeatWrapping
+
+// Bushes Textures
+const bushColorTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.webp')
+const bushARMTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.webp')
+const bushNormalTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_nor_gl_1k.webp')
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace
+
+bushColorTexture.wrapS = THREE.RepeatWrapping
+bushARMTexture.wrapS = THREE.RepeatWrapping
+bushNormalTexture.wrapS = THREE.RepeatWrapping
+
+//Grave Textures
 /**
  * House
  */
@@ -71,13 +99,21 @@ const walls = new THREE.Mesh(
 walls.position.y = 2.5 / 2
 house.add(walls)
 
+//Roof
 const roof = new THREE.Mesh(
     new THREE.ConeGeometry(3.5, 1.5, 4),
-    new THREE.MeshStandardMaterial()
+    new THREE.MeshStandardMaterial({
+        map: roofColorTexture,
+        aoMap: roofARMTexture,
+        roughnessMap: roofARMTexture,
+        metalnessMap: roofARMTexture,
+        normalMap: roofNormalTexture
+    })
 )
 roof.position.y = 2.5 + 0.75
 roof.rotation.y = Math.PI * 0.25
 house.add(roof)
+
 //Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20, 100, 100),
@@ -89,7 +125,7 @@ const floor = new THREE.Mesh(
         roughnessMap: floorARMTexture,
         metalnessMap: floorARMTexture,
         normalMap: floorNormalTexture,
-        displacementMap:floorDisplacementTexture,
+        displacementMap: floorDisplacementTexture,
         displacementScale: 0.3,
         displacementBias: -0.2,
     })
@@ -99,6 +135,7 @@ scene.add(floor)
 
 gui.add(floor.material, 'displacementScale').min(0).max(1).step(0.001).name('floorDisplacementScale')
 gui.add(floor.material, 'displacementBias').min(-1).max(1).step(0.001).name('floorDisplacementBias')
+
 //Door
 const door = new THREE.Mesh(
     new THREE.PlaneGeometry(2.2, 2.2),
@@ -110,27 +147,37 @@ house.add(door)
 
 //Bushes
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
-const bushMaterial = new THREE.MeshStandardMaterial()
+const bushMaterial = new THREE.MeshStandardMaterial({
+    color: '#ccffcc',
+    map: bushColorTexture,
+    aoMap: bushARMTexture,
+    roughnessMap: bushARMTexture,
+    metalnessMap: bushARMTexture,
+    normalMap: bushNormalTexture
+})
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush1.position.set(0.8, 0.2, 2.2)
 bush1.scale.setScalar(0.5, 0.5, 0.5)
+bush1.rotation.x = -0.75
 
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush2.position.set(1.4, 0.1, 2.1)
 bush2.scale.setScalar(0.25, 0.25, 0.25)
-
+bush2.rotation.x = -0.75
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush3.position.set(-0.8, 0.1, 2.2)
 bush3.scale.setScalar(0.4, 0.4, 0.4)
+bush3.rotation.x = -0.75
 
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush4.position.set(-1, 0.05, 2.6)
 bush4.scale.setScalar(0.15, 0.15, 0.15)
 house.add(bush1, bush2, bush3, bush4)
+bush4.rotation.x = -0.75
 
 //Graves
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
