5026c594876274d20bff72c8e268747279257df0
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-11-05 11:34:43 +0100

    added textures to graves

--- src/script.js
+++ src/script.js
@@ -77,6 +77,15 @@ bushARMTexture.wrapS = THREE.RepeatWrapping
 bushNormalTexture.wrapS = THREE.RepeatWrapping
 
 //Grave Textures
+const graveColorTexture = textureLoader.load('./grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.webp')
+const graveARMTexture = textureLoader.load('./grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.webp')
+const graveNormalTexture = textureLoader.load('./grave/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.webp')
+
+graveColorTexture.colorSpace = THREE.SRGBColorSpace
+
+graveColorTexture.repeat.set(0.3, 0.4)
+graveARMTexture.repeat.set(0.3, 0.4)
+graveNormalTexture.repeat.set(0.3, 0.4)
 /**
  * House
  */
@@ -139,7 +148,7 @@ gui.add(floor.material, 'displacementBias').min(-1).max(1).step(0.001).name('flo
 //Door
 const door = new THREE.Mesh(
     new THREE.PlaneGeometry(2.2, 2.2),
-    new THREE.MeshStandardMaterial({ color: 'red' })
+    new THREE.MeshStandardMaterial( )
 )
 door.position.y = 1
 door.position.z = 2 + 0.01
@@ -181,7 +190,13 @@ bush4.rotation.x = -0.75
 
 //Graves
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
-const graveMaterial = new THREE.MeshStandardMaterial()
+const graveMaterial = new THREE.MeshStandardMaterial({
+    map: graveColorTexture,
+    aoMap: graveARMTexture,
+    roughnessMap: graveARMTexture,
+    metalnessMap: graveARMTexture,
+    normalMap: graveNormalTexture
+})
 
 const graves = new THREE.Group(graveGeometry, graveMaterial)
 scene.add(graves)
